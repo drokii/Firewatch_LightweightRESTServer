07b93e1e0e81edf649f11707d177204792604fc2
Pedro Marques <pedrodossantosmarques@gmail.com>
2018-04-12 16:28:05 +0200

    Yadaydaydayda

--- LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/LoRaRestPoCApplication.java
+++ LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/LoRaRestPoCApplication.java
@@ -1,11 +1,14 @@
 package org.firewatch.apiserver.LoRa_REST_PoC;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class LoRaRestPoCApplication {
-
+	private static final org.slf4j.Logger log = LoggerFactory.getLogger(LoRaRestPoCApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(LoRaRestPoCApplication.class, args);
 	}
