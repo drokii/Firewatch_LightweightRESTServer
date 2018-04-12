5a84271205ea49388567414311c1d04226900963
Pedro Marques <pedrodossantosmarques@gmail.com>
2018-04-12 15:44:53 +0200

    And again

--- LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/LoRaController.java
+++ LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/LoRaController.java
@@ -1,19 +1,16 @@
 package org.firewatch.apiserver.LoRa_REST_PoC;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class LoRaController {
 
-    @RequestMapping(method = POST, value = "/broadcast", consumes = {"application/json"})
-    public void receiveBroadcast(@RequestParam("app_id") String test) {
+    @RequestMapping(method = POST, value = "/broadcast")
+    public void receiveBroadcast(@PathVariable String app_id, @RequestBody TestObject input) {
         try {
-            System.out.println(test);
+            System.out.println(app_id);
         } catch(Exception e) {
             System.out.println("Broadcast was recieved, but something went wrong: " + e.getMessage());
         }
