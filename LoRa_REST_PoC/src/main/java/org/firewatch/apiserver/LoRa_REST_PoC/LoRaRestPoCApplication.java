7c016da01660fde26a5ae5db7e8d599cae714f0f
Pedro Marques <pedrodossantosmarques@gmail.com>
2018-04-12 11:45:10 +0200

    Added spring boot rest server

---
+++ LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/LoRaRestPoCApplication.java
@@ -0,0 +1,12 @@
+package org.firewatch.apiserver.LoRa_REST_PoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoRaRestPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoRaRestPoCApplication.class, args);
+	}
+}
