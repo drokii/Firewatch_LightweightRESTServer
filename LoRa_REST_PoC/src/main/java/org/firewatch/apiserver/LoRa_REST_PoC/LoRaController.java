f34016d17008ef8dfa26349439b81068277bc4f6
Pedro Marques <pedrodossantosmarques@gmail.com>
2018-06-01 13:35:43 +0200

    Updated LORA POC to handle new test

--- LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/LoRaController.java
+++ LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/LoRaController.java
@@ -10,7 +10,7 @@ public class LoRaController {
     @RequestMapping(method = POST, value = "/broadcast")
     public void receiveBroadcast(@RequestBody UplinkData data) {
         try {
-            System.out.println(data.getAppId());
+            System.out.println(data.getPayloadFields().getHeatPercentageInPicture());
         } catch(Exception e) {
             System.out.println("Broadcast was recieved, but something went wrong: " + e.getMessage());
         }
--- LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/PayloadFields.java
+++ LoRa_REST_PoC/src/main/java/org/firewatch/apiserver/LoRa_REST_PoC/PayloadFields.java
@@ -2,17 +2,18 @@ package org.firewatch.apiserver.LoRa_REST_PoC;
 
 import java.util.HashMap;
 import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import com.fasterxml.jackson.annotation.*;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
 
 })
 public class PayloadFields {
+
+
+    @JsonProperty("heatPercentageInPicture")
+    private int heatPercentageInPicture;
 
     @JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
@@ -26,5 +27,16 @@ public class PayloadFields {
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
     }
+
+    @JsonAnyGetter
+    public int getHeatPercentageInPicture() {
+        return heatPercentageInPicture;
+    }
+
+    @JsonAnySetter
+    public void setHeatPercentageInPicture(int heatPercentageInPicture) {
+        this.heatPercentageInPicture = heatPercentageInPicture;
+    }
+
 
 }
